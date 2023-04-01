c291315a52d443b9838313f13323303b01888631
dass300 <dass300@gmail.com>
2023-04-01 21:47:32 +0530

    first commit

---
+++ src/Components/Content.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div>
+    <div className=''>
+    <h3></h3>
+    <p></p>
+    </div>
+    </div>
+  )
+}
+
+export default Content
+
+
+
+
+
