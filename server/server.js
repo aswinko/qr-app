8ebe43e78fa191e8310462961ca89a2f4cdf9996
Aswin K O <aswinko479@gmail.com>
2023-04-25 09:22:54 +0530

    new cors

--- server/server.js
+++ server/server.js
@@ -13,6 +13,18 @@ app.use(cors({
   origin: true,
   credentials: true
 }));
+
+app.use(function(req,res,next) {
+  req.socket.setNoDelay(true)
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", true);
+      res.header("Access-Control-Allow-Origin", "https://aswinko.github.io/qr-app-frontend/"); 
+
+  res.header('Access-Control-Expose-Headers', 'agreementrequired');
+
+  next()
+})
+
 app.use(express.json());
 app.use(cookieParser());
 
