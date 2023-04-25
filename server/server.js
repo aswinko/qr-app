622d48a63082da784d74a0db07c606f082d19aa5
Aswin K O <aswinko479@gmail.com>
2023-04-25 09:48:24 +0530

    all

--- server/server.js
+++ server/server.js
@@ -18,7 +18,13 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    res.send(200);
+  } else {
+    next();
+  }
 });
 
 app.use(express.json());
